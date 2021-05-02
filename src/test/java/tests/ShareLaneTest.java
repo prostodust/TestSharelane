759b8d7b464306d755f7c8e9bb4a4982a150d60a
Oleg <prostodust@gmail.com>
2021-05-02 14:03:16 +0300

    Update test in ShareLaneTest

--- src/test/java/tests/ShareLaneTest.java
+++ src/test/java/tests/ShareLaneTest.java
@@ -3,29 +3,12 @@ package tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 public class ShareLaneTest {
 
-// IE
-//        System.setProperty("webdriver.ie.driver", "src/test/resources/IEDriverServer.exe");
-//        WebDriver driver = new InternetExplorerDriver();
-// Edge
-//        System.setProperty("webdriver.edge.driver", "src/test/resources/msedgedriver.exe");
-//        EdgeDriver driver = new EdgeDriver();
-// Yandex
-//        System.setProperty("webdriver.chrome.driver", "src/test/resources/yandexdriver.exe");
-//        WebDriver driver = new ChromeDriver();
-// Chrome
-//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-//        WebDriver driver = new ChromeDriver();
-
-
     @Test
     public void fillZipcodeFildTest() {
-
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
@@ -37,7 +20,6 @@ public class ShareLaneTest {
 
     @Test
     public void fillSighUpFildTest() {
-
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
